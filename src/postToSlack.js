875c969f56f293219cf2d13ff22b6f7bb5c32a76
Elmer Fudd <fudd_github@elmerfamily.com>
2019-09-22 01:56:49 -0500

    work on postToSlack

--- src/postToSlack.js
+++ src/postToSlack.js
@@ -1,34 +1,52 @@
 // src/postToSlack.js
+const request = require('request');
+
 const postToSlack = (slackURL, message) => {
   console.log('Posting to Slack');
-  const req = request({
-    url: slackURL,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({text:message}),
-    resolveWithFullResponse: true
+
+  request.post(slackURL, {
+    json: {
+      text: message
+    } }, (error, res, body) => {
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    console.log(`statusCode: ${res.statusCode}`);
+    console.log(body);
   });
 
-  req
-    .then((response) => {
-      const result = JSON.parse(response.body);
-      console.log(`result: ${result}`);
-      var k = Object.keys(result);
-      console.log(`Result properties: ${k}`);
-      k = Object.keys(response);
-      console.log(`Response properties: ${k}`);
-      resolve(result);
-    })
-    .catch((err) => {
-      console.log(`Error response: ${err.response}`);
-      var k = Object.keys(err.response);
-      console.log(`Response properties: ${k}`);
-      console.log(`Status Message: ${err.statusMessage}`);
-      console.log(`Request: ${err.request}`);
-      console.log(`Body: ${err.body}`);
-    });
+  return;
+  // const req = request({
+  //   url: slackURL,
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.stringify({text:message}),
+  //   resolveWithFullResponse: true
+  // });
+
+  // req
+  //   .then((response) => {
+  //     const result = JSON.parse(response.body);
+  //     console.log(`result: ${result}`);
+  //     var k = Object.keys(result);
+  //     console.log(`Result properties: ${k}`);
+  //     k = Object.keys(response);
+  //     console.log(`Response properties: ${k}`);
+  //     resolve(result);
+  //   })
+  //   .catch((err) => {
+  //     console.log(`Error response: ${err.response}`);
+  //     var k = Object.keys(err.response);
+  //     console.log(`Response properties: ${k}`);
+  //     console.log(`Status Message: ${err.statusMessage}`);
+  //     console.log(`Request: ${err.request}`);
+  //     console.log(`Body: ${err.body}`);
+  //   });
 
 };
 
