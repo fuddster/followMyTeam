dbf1392e87bfe26ba66426b4f6d8dc3c8c5f2d37
Elmer Fudd <fudd_github@elmerfamily.com>
2019-09-24 01:15:22 -0500

    test match score formatting

--- src/matchScore.js
+++ src/matchScore.js
@@ -25,7 +25,7 @@ const parseMatchScoreEvent = (body) => {
   return {
     "type": "section",
     "text": {
-      "text": 'Match Number: ',
+      "text": "Match Number: ",
       "type": "mrkdwn"
     }};
   var txt =
