f3a5a7273fbb02e43c95f155c6fc1f22c8e6e700
Elmer Fudd <fudd_github@elmerfamily.com>
2019-09-24 20:27:19 -0500

    test match score formatting

--- src/postToSlack.js
+++ src/postToSlack.js
@@ -19,7 +19,7 @@ const postToSlack = (slackURL, message) => {
     console.log(`statusCode: ${res.statusCode}`);
     console.log(`statusMessage: ${res.statusMessage}`);
     console.log(`headers: ${Object.keys(res.headers)}`);
-    console.log(`headers: ${res.headers.content-type}`);
+    console.log(`headers: ${JSON.stringify(res.headers, null, 4)}`);
     console.log(`keys = ${Object.keys(res)}`);
     console.log(`body2: ${body}`);
   });
