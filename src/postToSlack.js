8d3dc6ba924a592a3a0227e699359fdb839e1326
Elmer Fudd <fudd_github@elmerfamily.com>
2019-09-25 16:00:07 -0500

    test match score formatting

--- src/postToSlack.js
+++ src/postToSlack.js
@@ -10,12 +10,12 @@ const postToSlack = (slackURL, message) => {
   //var payload = { json: message };
   //var payload = JSON.stringify({ text: "This is a test!" });
   //var payload = { text: "This is a test!" };
-  var payload = {
-  type: "section",
-  text: {
-    type: "mrkdwn",
-    text: "A message *with some bold text* and _some italicized text_."
-  }};
+  var payload = { "blocks": {
+  "type": "section",
+  "text": {
+    "type": "mrkdwn",
+    "text": "A message *with some bold text* and _some italicized text_."
+  }}};
 
   //request.post({url: slackURL, headers: headers, payload: payload}, (error, res, body) => {
   request.post({
@@ -32,8 +32,7 @@ const postToSlack = (slackURL, message) => {
     //console.log(`request: ${request}`)
     console.log(`statusCode: ${res.statusCode}`);
     console.log(`statusMessage: ${res.statusMessage}`);
-    console.log(`headers: ${JSON.stringify(res.headers, null, 4)}`);
-    console.log(`keys = ${JSON.stringify(res,null,4)}`);
+    console.log(`res = ${JSON.stringify(res,null,4)}`);
     console.log(`body2: ${body}`);
     console.log(`error: ${JSON.stringify(error, null, 4)}`);
   });
