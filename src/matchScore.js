0a54e03cc225c0a46d252602e149f797247d8889
Elmer Fudd <fudd_github@elmerfamily.com>
2019-09-28 12:22:11 -0500

    strip frc from start of team name

--- src/matchScore.js
+++ src/matchScore.js
@@ -5,13 +5,13 @@ const parseMatchScoreEvent = (body) => {
   const matchNumber = body.message_data.match.match_number;
   const eventName = body.message_data.event_name;
   const blueAllianceScore = body.message_data.match.alliances.blue.score;
-  const blueAlliance1 = body.message_data.match.alliances.blue.teams[0];
-  const blueAlliance2 = body.message_data.match.alliances.blue.teams[1];
-  const blueAlliance3 = body.message_data.match.alliances.blue.teams[2];
+  const blueAlliance1 = body.message_data.match.alliances.blue.teams[0].substr(3);
+  const blueAlliance2 = body.message_data.match.alliances.blue.teams[1].substr(3);
+  const blueAlliance3 = body.message_data.match.alliances.blue.teams[2].substr(3);
   const redAllianceScore = body.message_data.match.alliances.red.score;
-  const redAlliance1 = body.message_data.match.alliances.red.teams[0];
-  const redAlliance2 = body.message_data.match.alliances.red.teams[1];
-  const redAlliance3 = body.message_data.match.alliances.red.teams[2];
+  const redAlliance1 = body.message_data.match.alliances.red.teams[0].substr(3);
+  const redAlliance2 = body.message_data.match.alliances.red.teams[1].substr(3);
+  const redAlliance3 = body.message_data.match.alliances.red.teams[2].substr(3);
 
   var opening = eventName + '\nMatch Number: ' + matchNumber + '\nResult:';
   var score = '*Score*:  ' +  blueAllianceScore + ' - ' + redAllianceScore;
